9ed4e1256eb6f9f7057a2771b2f9f867e0066949
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-03 23:59:53 +0800

    push

--- login.php
+++ login.php
@@ -3,19 +3,12 @@ session_start();
 
 $id_pengguna = $_POST['id_pengguna'];
 $kata_laluan = $_POST['kata_laluan'];
-
 if ($id_pengguna == "admin" && $kata_laluan == "admin") {
-
     $_SESSION['id_pengguna'] = $id_pengguna;
-
     header("Location: admin.php");
     exit();
-
 } else {
-
     header("Location: index.php");
     exit();
-
 }
-
 ?>
