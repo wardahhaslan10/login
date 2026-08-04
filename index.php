c2cee35fa0eec8abb92ebbf92dd118a1d18921d5
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-04 16:59:11 +0800

    push

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
 <h2>Login Pengguna</h2>
 <form action="login.php" method="POST">
-    <label>ID Pengguna:</label>
-    <input type="text" name="id_pengguna">
-    <br><br>
-    <label>Kata Laluan:</label>
-    <input type="password" name="kata_laluan">
-    <br><br>
-    <button type="submit">Log Masuk</button>
+<label for="id_pengguna">ID Pengguna:</label>
+<input type="text" id="id_pengguna" name="id_pengguna">
+<br><br>
+<label>Kata Laluan:</label>
+<input type="password" name="kata_laluan">
+<br><br>
+<button type="submit">Log Masuk</button>
 </form>
 </body>
 </html>
