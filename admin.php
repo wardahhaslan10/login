51f44fc0cb84d7270998a9310c0ff946bd37b031
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-04 16:48:19 +0800

    push

--- admin.php
+++ admin.php
@@ -8,8 +8,10 @@ if (!isset($_SESSION['id_pengguna'])) {
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
 </head>
 <body>
