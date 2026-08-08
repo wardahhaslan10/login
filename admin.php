b6afb1640ead000efd05897864d41b755dfd503f
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-08 16:43:18 +0800

    push

--- admin.php
+++ admin.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -14,53 +13,16 @@ if (!isset($_SESSION['id_pengguna']) || $_SESSION['id_pengguna'] != "admin") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
 
 <div class="container mt-5">
-
     <div class="alert alert-success text-center">
-
         <h2>Selamat Datang Admin</h2>
-
-        <p>
-            Anda berjaya login sebagai Admin.
-        </p>
+        <p>Anda berjaya login sebagai Admin.</p>
 
     </div>
-
 </div>
-
 </body>
-=======
-<?php
-session_start();
-
-if (!isset($_SESSION['id_pengguna'])) {
-    header("Location: index.php");
-    exit();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-<body>
-
- <h2>Selamat Datang Admin</h2>
-    <p>
-        ID Pengguna:
-        <?php echo $_SESSION['id_pengguna']; ?>
-    </p>
-</body>
->>>>>>> 69174441c72b3d861e157e454a150dd934c13b86
 </html>
