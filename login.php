69174441c72b3d861e157e454a150dd934c13b86
wardahhaslan10 <wardahhaslan01@gmail.com>
2026-08-08 15:54:12 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$id_pengguna = $_POST['id_pengguna'];
+$kata_laluan = $_POST['kata_laluan'];
+
+if ($id_pengguna == "admin" && $kata_laluan == "admin") {
+
+    $_SESSION['id_pengguna'] = $id_pengguna;
+
+    header("Location: admin.php");
+    exit();
+
+} elseif ($id_pengguna == "staf" && $kata_laluan == "staf") {
+
+    $_SESSION['id_pengguna'] = $id_pengguna;
+
+    header("Location: staf.php");
+    exit();
+
+} else {
+
+    header("Location: index.php");
+    exit();
+
+}
+?>
