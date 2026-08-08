e7d3f26c4d1201ae1b14fa3a299cf490c8607b67
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-08 16:44:40 +0800

    push

--- staf.php
+++ staf.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -21,48 +20,12 @@ if (!isset($_SESSION['id_pengguna']) || $_SESSION['id_pengguna'] != "staf") {
 <body>
 
 <div class="container mt-5">
-
     <div class="alert alert-info text-center">
 
         <h2>Selamat Datang Staf</h2>
 
-        <p>
-            Anda berjaya login sebagai Staf.
-        </p>
-
+        <p>Anda berjaya login sebagai Staf.</p>
     </div>
-
 </div>
-
 </body>
-=======
-<?php
-session_start();
-
-if (!isset($_SESSION['id_pengguna'])) {
-
-    header("Location: index.php");
-    exit();
-
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Staff</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
-<body>
-
-     <h2>Selamat Datang Staf</h2>
-    <p>
-        ID Pengguna:
-        <?php echo $_SESSION['id_pengguna']; ?>
-    </p>    
-</body>
->>>>>>> 69174441c72b3d861e157e454a150dd934c13b86
 </html>
