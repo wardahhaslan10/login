aac1531a6194e57395f2bda2bb7d2ad1800b2ff5
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-10 09:57:53 +0800

    push

--- login.php
+++ login.php
@@ -7,33 +7,23 @@ $kata_laluan = $_POST['kata_laluan'];
 
 // KEDUA-DUA RUANG KOSONG
 if ($id_pengguna == "" && $kata_laluan == "") {
-
     $_SESSION['ralat'] = "Sila isi ID Pengguna dan Kata Laluan.";
-
     header("Location: index.php");
     exit();
 }
-
 
 // ID PENGGUNA KOSONG
 elseif ($id_pengguna == "") {
-
     $_SESSION['ralat'] = "Sila isi ID Pengguna.";
-
     header("Location: index.php");
     exit();
-
 }
-
 
 // KATA LALUAN KOSONG
 elseif ($kata_laluan == "") {
-
     $_SESSION['ralat'] = "Sila isi Kata Laluan.";
-
     header("Location: index.php");
     exit();
-
 }
 
 // LOGIN ADMIN
@@ -41,7 +31,6 @@ elseif ($id_pengguna == "admin" && $kata_laluan == "admin") {
     $_SESSION['id_pengguna'] = "admin";
     header("Location: admin.php");
     exit();
-
 }
 
 // LOGIN STAF
@@ -49,7 +38,6 @@ elseif ($id_pengguna == "staf" && $kata_laluan == "staf") {
     $_SESSION['id_pengguna'] = "staf";
     header("Location: staf.php");
     exit();
-
 }
 
 // ID SALAH
@@ -64,6 +52,5 @@ else {
     $_SESSION['ralat'] = "Kata Laluan salah.";
     header("Location: index.php");
     exit();
-
 }
 ?>
