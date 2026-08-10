2f1ed25909aa865be70febf9b7a861b2ff9e7be8
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-08-10 09:14:53 +0800

    push

--- index.php
+++ index.php
@@ -14,9 +14,7 @@ if (isset($_SESSION['ralat'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Login</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"rel="stylesheet">
 </head>
 
